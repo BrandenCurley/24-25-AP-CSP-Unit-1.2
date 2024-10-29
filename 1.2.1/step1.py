cad2ca0bff62b4605e9ef3ab9b72a563ab49a7af
13233 <branden.curley@vtsd.com>
2024-10-29 13:32:40 -0400

    1.2.1

--- 1.2.1/step1.py
+++ 1.2.1/step1.py
@@ -9,11 +9,14 @@ spot_shape = "circle"
 
 #-----initialize turtle-----
 spot = trtl.Turtle()
+spot.shape(spot_shape)
 spot.shapesize(spot_size)
 spot.fillcolor(spot_color)
 #-----game functions--------
-
+def spot_clicked(x, y):
+    print("hello world!")
 
 #-----events----------------
+spot.onclick(spot_clicked)
 wn = trtl.Screen()
 wn.mainloop()
