d817885d849dbb1e94ac25e9ac352d26ede2562a
mrkiedes <akiedes@vtsd.com>
2024-12-06 12:52:54 -0500

    1.2.4 Complete

--- 1-2-4/main.py
+++ 1-2-4/main.py
@@ -1,9 +1,9 @@
 import turtle as trtl
+import random as rand
 
 # Intialize Variables
 wn = trtl.Screen()
 maze_painter = trtl.Turtle()
-length = 35
 path_width = 30
 
 
@@ -24,26 +24,25 @@ def draw_barrier():
     maze_painter.backward(path_width)
     maze_painter.left(90)
 
-
-
+length = 35
 for wall in range(21):
-    maze_painter.forward(length/3)
+    gap_space = rand.randint(0, length - path_width)
+    door_space = 0
+    maze_painter.forward(gap_space)
     maze_painter.penup()
     maze_painter.forward(path_width)
     maze_painter.pendown()
     if(wall > 5):
-        draw_barrier()
-    maze_painter.forward(length- path_width - (length/3))
+       door_space = rand.randint(0, length - path_width - gap_space)
+       maze_painter.forward(door_space)
+       draw_barrier()
+    maze_painter.forward(length - gap_space - path_width - door_space)
     maze_painter.left(90)
     length += 15
 
 
 
-
-
-
-
-
 wn.listen()
 wn.mainloop()
+
 
